169f26d158aed6d55229acccfe22d249db628b36
golden.li <magic.lixin@gmail.com>
2022-07-25 20:26:36 +0800

    make it works on M1

--- rlog_reader/cereal/install_capnp.sh
+++ rlog_reader/cereal/install_capnp.sh
@@ -18,7 +18,12 @@ g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -D
 g++ -std=gnu++11 -I./src -I./src -DKJ_HEADER_WARNINGS -DCAPNP_HEADER_WARNINGS -DCAPNP_INCLUDE_DIR=\"/usr/local/include\" -pthread -O2 -DNDEBUG -pthread -pthread -o .libs/capnpc-capnp src/capnp/compiler/capnpc-capnp.o  ./.libs/libcapnp.a ./.libs/libkj.a -lpthread -pthread
 
 sudo cp .libs/capnp /usr/local/bin/
-sudo rm /usr/local/bin/capnpc
+
+file=/usr/local/bin/capnpc
+if test -f "$file"; then
+    sudo rm $file
+fi
+
 sudo ln -s /usr/local/bin/capnp /usr/local/bin/capnpc
 sudo cp .libs/capnpc-c++ /usr/local/bin/
 sudo cp .libs/capnpc-capnp /usr/local/bin/
@@ -26,7 +31,12 @@ sudo cp .libs/*.a /usr/local/lib
 
 cd /tmp
 echo "Installing c-capnp"
-rm -rf c-capnproto/
+
+file=c-capnproto/
+if test -d "$file"; then
+    rm -rf $file
+fi
+
 git clone https://github.com/commaai/c-capnproto.git
 cd c-capnproto
 git submodule update --init --recursive
@@ -39,3 +49,5 @@ gcc -fPIC -o .libs/capnpc-c compiler/capnpc-c.o compiler/schema.capnp.o compiler
 
 sudo cp .libs/capnpc-c /usr/local/bin/
 sudo cp .libs/*.a /usr/local/lib
+
+echo "install capnpc finished !!! :)"
